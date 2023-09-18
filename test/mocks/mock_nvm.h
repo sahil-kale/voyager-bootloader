8462bd81db139c474cd841eab82c8f8d5345d5ed
Sahil Kale <sahil.kale1@gmail.com>
2023-09-18 23:53:13 +0200

    [Bootloader] Remove option to feature flag flash verification before boot
    Enforce the need to verify flash before boot as the bootloader
    may be put into weird situations on disconnect and can permit corrupted
    flash to execute
    
    Topic: enforce-flash-verification
    Relative:
    Reviewers:

--- test/mocks/mock_nvm.h
+++ test/mocks/mock_nvm.h
@@ -9,7 +9,6 @@ typedef struct {
     voyager_bootloader_addr_size_t app_start_address;
     voyager_bootloader_addr_size_t app_end_address;
     voyager_bootloader_app_size_t app_size;
-    voyager_bootloader_verify_flash_before_jumping_t verify_flash_before_jumping;
 } mock_nvm_data_t;
 
 mock_nvm_data_t *mock_nvm_get_data(void);
